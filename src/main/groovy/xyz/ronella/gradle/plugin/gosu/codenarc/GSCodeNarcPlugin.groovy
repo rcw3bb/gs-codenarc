f250e070bb90fa2f0523b087faad209df0671054
Ron Webb <ron@ronella.xyz>
2022-02-26 21:02:48 +1300

    Update gs-codenarc-ext

--- src/main/groovy/xyz/ronella/gradle/plugin/gosu/codenarc/GSCodeNarcPlugin.groovy
+++ src/main/groovy/xyz/ronella/gradle/plugin/gosu/codenarc/GSCodeNarcPlugin.groovy
@@ -13,7 +13,7 @@ class GSCodeNarcPlugin implements Plugin<Project> {
 
     private final Logger LOG = Logging.getLogger(GSCodeNarc.class)
 
-    public static final String DEFAULT_GSCODENARC_VERSION = "1.0.0"
+    public static final String DEFAULT_GSCODENARC_VERSION = "1.1.1"
 
     protected def createAntTask(Project project) {
         withBasePlugin(project) {
